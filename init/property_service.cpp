bfc151b9d7b7ff3f5d4a90681f6c69a05d2f0960
Tim Zimmermann <tim@linux4.de>
2022-06-03 20:01:38 +0200

    init: Also override ro.boot.warranty_bit
    Change-Id: Id7c1d4e623f352a57c08625cae08fc330d7ddb27

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1397,6 +1397,7 @@ static void SetSafetyNetProps() {
     InitPropertySet("ro.boot.verifiedbootstate", "green");
     InitPropertySet("ro.boot.veritymode", "enforcing");
     InitPropertySet("ro.boot.vbmeta.device_state", "locked");
+    InitPropertySet("ro.boot.warranty_bit", "0");
 }
 
 void PropertyInit() {
